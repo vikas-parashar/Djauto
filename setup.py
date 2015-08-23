8403075e352f396dd9654bcdf3f288a7bdfac54d
vikas-parashar <svnitvikas@gmail.com>
2015-08-23 13:19:28 +0530

    setup.py updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='djauto',
+      version='0.1',
+      description='Django project generator',
+      url='https://github.com/vikas-parashar/django-project-automator',
+      author='Vikas Parashar',
+      author_email='svnitvikas@gmai.com',
+      license='MIT',
+      packages=['djauto'],
+      zip_safe=False)

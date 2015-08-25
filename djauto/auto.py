9efb42fe680e1d7199d5dd90f7aa111afdbd8326
vikas-parashar <svnitvikas@gmail.com>
2015-08-26 01:03:58 +0530

    pep8 format done

--- djauto/auto.py
+++ djauto/auto.py
@@ -15,36 +15,38 @@ fileout = base+"/"+project_name+"/"+project_name+"/settings.py"
 
 
 class Chdir:
-      def __init__( self, newPath ):
+
+    def __init__(self, newPath):
         self.savedPath = os.getcwd()
         os.chdir(newPath)
 
-      def __del__( self ):
-        os.chdir( self.savedPath )
+    def __del__(self):
+        os.chdir(self.savedPath)
+
 
 def add_installed_app(filename):
-	f = open(filename,'r')
-	filedata = f.read()
-	f.close()
-	olddata = "'django.contrib.staticfiles',"
-	newdata = "'django.contrib.staticfiles',\n\t"+"'"+app_name+"',"
-	replaced_data = filedata.replace(olddata,newdata)
+    f = open(filename, 'r')
+    filedata = f.read()
+    f.close()
+    olddata = "'django.contrib.staticfiles',"
+    newdata = "'django.contrib.staticfiles',\n\t"+"'"+app_name+"',"
+    replaced_data = filedata.replace(olddata, newdata)
 
-	f = open(fileout,'w')
-	f.write(replaced_data)
-	f.close()
+    f = open(fileout, 'w')
+    f.write(replaced_data)
+    f.close()
+
 
 def main():
-	# print "installing django.."
-	subprocess.call("pip install django", shell=True)
+    # print "installing django.."
+    subprocess.call("pip install django", shell=True)
 
-	# print "creating project", project_name, "..."
-	subprocess.call(new_project+project_name, shell=True)
-	os.chdir(project_name)
+    # print "creating project", project_name, "..."
+    subprocess.call(new_project+project_name, shell=True)
+    os.chdir(project_name)
 
-	# print "creating app..."
-	subprocess.call(new_app+app_name, shell=True)
-	add_installed_app(filename)
+    # print "creating app..."
+    subprocess.call(new_app+app_name, shell=True)
+    add_installed_app(filename)
 
-
-	print "We're done!"
+    print "We're done!"

a78643afd1e6e333c3221634ad0cd5428e53e690
vikas-parashar <svnitvikas@gmail.com>
2015-08-28 04:29:54 +0530

    updated

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(name='djauto',
               'Natural Language :: English',
               'Programming Language :: Python :: 2.6',
               'Programming Language :: Python :: 2.7',
-              'Programming Language :: Python :: 3'
+              'Programming Language :: Python :: 3',
               'Framework :: Django',
               'License :: OSI Approved :: MIT License'
           ],

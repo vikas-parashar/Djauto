74955e9145447f56b175f3b917b47d6f016f01cd
vikas-parashar <svnitvikas@gmail.com>
2015-09-02 01:50:08 +0530

    removed python 3.x support for now

--- djauto/auto.py
+++ djauto/auto.py
@@ -1,10 +1,7 @@
-from __future__ import print_function
-from builtins import input
-from builtins import object
 import os
 import subprocess
 import sys
-# import djauto
+import djauto
 
 
 project_name = sys.argv[1]
@@ -24,7 +21,7 @@ u3 = "\", \"description\":\""
 u4 = "\"}\'"
 add_repo = "git remote add origin git@github.com:"
 
-class Chdir(object):
+class Chdir:
 
     def __init__(self, newPath):
         self.savedPath = os.getcwd()
@@ -47,18 +44,17 @@ def add_installed_app(filename):
     f.close()
 
 def create_repo(username, repo_name, repo_description):
-	subprocess.call('git init', shell=True)
-	subprocess.call(u1+username+u2+repo_name+u3+repo_description+u4,
-					 shell=True)
-	subprocess.call(add_repo+username+"/"+repo_name+".git", shell=True)
-	subprocess.call('git add .', shell=True)
-	subprocess.call('git commit -m \"initial commit\"', shell=True)
-	subprocess.call("git push origin master", shell=True)
-	print("created")
+    subprocess.call('git init', shell=True)
+    subprocess.call(u1+username+u2+repo_name+u3+repo_description+u4,
+                     shell=True)
+    subprocess.call(add_repo+username+"/"+repo_name+".git", shell=True)
+    subprocess.call('git add .', shell=True)
+    subprocess.call('git commit -m \"initial commit\"', shell=True)
+    subprocess.call("git push origin master", shell=True)
+    print "created"
 
 def main():
     # print "installing django.."
-    subprocess.call('pip install future', shell= True)
     subprocess.call("pip install django", shell=True)
 
     # print "creating project", project_name, "..."
@@ -69,11 +65,11 @@ def main():
     subprocess.call(new_app+app_name, shell=True)
     add_installed_app(filename)
     # create_repo(username, repo_name)
-    prompt = input("Do you want to create repo on github [y/n]: ")
+    prompt = raw_input("Do you want to create repo on github [y/n]: ")
     if prompt =='y' or prompt == 'Y':
-    	username = input("Enter github username: ")
-    	# repo_name = raw_input("Enter valid repo name: ")
-    	repo_description = input("Enter short description for repo: ")
-    	create_repo(username, project_name, repo_description)
+        username = raw_input("Enter github username: ")
+        # repo_name = raw_input("Enter valid repo name: ")
+        repo_description = raw_input("Enter short description for repo: ")
+        create_repo(username, project_name, repo_description)
 
-    print("We're done!")
+    print "We're done!"
